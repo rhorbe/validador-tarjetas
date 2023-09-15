a2ba2e35a2e735a0735c6887ada5c0a6433ba71a
Rafael Orbe <rhorbe@hotmail.com>
2023-09-15 11:05:50 -0300

    Mejoras en la logica  y la interfaz

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,7 @@
 const TARJETA_INVALIDA = 0;
 const TARJETA_NO_RECONOCIDA = 1;
 const TARJETA_VISA = 2;
+const TARJETA_DINERS = 3;
 
 const numTarjeta = document.getElementById("numTarjeta");
 const feedback = document.getElementById("feedback");
@@ -8,11 +9,11 @@ const btnRetablecer = document.getElementById("btnRetablecer");
 const iconoTarjetaDesconocida = document.getElementById(
   "iconoTarjetaDesconocida"
 );
-const iconoTarjetaVisa = document.getElementById("iconoVisa");
+const iconoVisa = document.getElementById("iconoVisa");
+const iconoDiners = document.getElementById("iconoDiners");
 
 numTarjeta.addEventListener("input", manejadorValidacion);
 btnRetablecer.addEventListener("click", restablecerFormulario);
-
 
 function manejadorValidacion(event) {
   let tarjetaDetectada = detectarTarjeta();
@@ -22,13 +23,11 @@ function manejadorValidacion(event) {
   else aceptarTarjeta(tarjetaDetectada);
 }
 
-
 function mostrarError() {
   limpiarErrores();
   numTarjeta.classList.add("is-invalid");
   feedback.innerHTML = "S&oacutelo puede ingresar s&oacutelo n&uacutemeros";
 }
-
 
 function aceptarTarjeta(tarjetaDetectada) {
   limpiarErrores();
@@ -37,11 +36,13 @@ function aceptarTarjeta(tarjetaDetectada) {
   mostrarIconoTarjetaDetectada(tarjetaDetectada);
 }
 
-
 function mostrarIconoTarjetaDetectada(tarjetaDetectada) {
   switch (tarjetaDetectada) {
     case TARJETA_VISA:
-      iconoTarjetaVisa.classList.remove("d-none");
+      iconoVisa.classList.remove("d-none");
+      break;
+    case TARJETA_DINERS:
+      iconoDiners.classList.remove("d-none");
       break;
 
     default:
@@ -50,18 +51,15 @@ function mostrarIconoTarjetaDetectada(tarjetaDetectada) {
   }
 }
 
-
 function ocultarIconosTarjetas() {
   iconoTarjetaDesconocida.classList.add("d-none");
-  iconoTarjetaVisa.classList.add("d-none");
+  iconoVisa.classList.add("d-none");
 }
-
 
 function limpiarErrores() {
   numTarjeta.classList.remove("is-invalid");
   numTarjeta.classList.remove("is-valid");
 }
-
 
 function restablecerFormulario() {
   limpiarErrores();
@@ -70,21 +68,30 @@ function restablecerFormulario() {
   numTarjeta.value = "";
 }
 
-
 function restablecerIconosTarjetas() {
+  alert("restablecer iconos");
   ocultarIconosTarjetas();
   iconoTarjetaDesconocida.classList.remove("d-none");
 }
-
 
 function detectarTarjeta() {
   if (
     numTarjeta.value == "" ||
     numTarjeta.value == "v" ||
     numTarjeta.value == "vi" ||
-    numTarjeta.value == "vis"
-  )
+    numTarjeta.value == "vis" ||
+    numTarjeta.value == "d" ||
+    numTarjeta.value == "di" ||
+    numTarjeta.value == "din" ||
+    numTarjeta.value == "dine" ||
+    numTarjeta.value == "diner"
+  ) {
     return TARJETA_NO_RECONOCIDA;
-  else if (numTarjeta.value == "visa") return TARJETA_VISA;
-  else return TARJETA_INVALIDA;
+  } else if (numTarjeta.value == "visa") {
+    return TARJETA_VISA;
+  } else if (numTarjeta.value == "diners") {
+    return TARJETA_DINERS;
+  } else {
+    return TARJETA_INVALIDA;
+  }
 }
